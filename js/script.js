997c4adebecbb7745c556769fc009da77f8a736d
hisbanshiraz <hisbanbardai@gmail.com>
2024-05-09 11:27:25 -0600

    Add script to make mobile navigation work

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+//Set current year in footer
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+//Make mobile navigation work
+const navBtnEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+navBtnEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
